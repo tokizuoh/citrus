36e6c5d0653f999e1a4c7dc615deeaf0cb88398e
tokizuoh <tokizuoh@gmail.com>
2023-02-25 13:50:48 +0900

    rm template

--- Sources/citrus/citrus.swift
+++ Sources/citrus/citrus.swift
@@ -2,8 +2,6 @@ import Foundation
 
 @main
 public struct citrus {
-    public private(set) var text = "Hello, World!"
-
     public static func main() {
         let client = Client()
         client.query()
