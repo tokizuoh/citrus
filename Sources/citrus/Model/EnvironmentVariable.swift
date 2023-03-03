0c88b65091930d40da96b63624cec4463527c90b
tokizuoh <tokizuoh@gmail.com>
2023-03-03 23:54:39 +0900

    separate file

---
+++ Sources/citrus/Model/EnvironmentVariable.swift
@@ -0,0 +1,13 @@
+//
+//  EnvironmentVariable.swift
+//  
+//
+//  Created by tokizo on 2023/03/03.
+//
+
+enum EnvironmentVariable {
+    static var name: String?
+    static var owner: String?
+    static var token: String?
+    static var expression: String?
+}
